f2444184a5b1be08f0724af10a2fb5f9a21a8fc7
Luis Arroyo <Luis.Arroyo@xerox.com>
2022-06-01 00:15:49 -0600

    Added routes and theme

--- src/App.js
+++ src/App.js
@@ -2,12 +2,47 @@ import logo from "./logo.svg";
 import "./App.css";
 import "@fontsource/roboto-condensed";
 import Login from "./login";
+import Twofa from "./twofa";
+import Dashboard from "./dashboard";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "./navbar";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 function App() {
-  const theme = {
-    spacing: 8,
-  };
-  return <div className="App"></div>;
+  const theme = createTheme({
+    typography: {
+      fontFamily: "Roboto Condensed",
+    },
+    palette: {
+      primary: {
+        light: "#ffffff",
+        main: "#fff",
+        dark: "#cccccc",
+        contrastText: "#000",
+      },
+      secondary: {
+        light: "#ff5d54",
+        main: "#db1e2a",
+        dark: "#a10001",
+        contrastText: "#fff",
+      },
+    },
+  });
+  if (!localStorage.token) {
+    return <Login />;
+  }
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="twofa" element={<Twofa />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="navbar" element={<NavBar />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
