192b18b8d5287d3ac0d91f5005640fb3bb46900d
Luis Arroyo <Luis.Arroyo@xerox.com>
2022-06-01 00:16:26 -0600

    General UI improvements

--- src/login.jsx
+++ src/login.jsx
@@ -1,83 +1,71 @@
-import React, { Component } from "react";
+import React from "react";
+import { useState } from "react";
 import Button from "@mui/material/Button";
 import { TextField, Grid, Box } from "@mui/material";
 import LoginSharpIcon from "@mui/icons-material/LoginSharp";
 import { login } from "./api/auth";
+import { useNavigate } from "react-router-dom";
 
-class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.reset();
-  }
-
-  reset() {
-    this.state = {
-      email: "luis.arroyo@xerox.com",
-      password: "",
-    };
-  }
-
-  async handleClick(event) {
-    event.preventDefault();
-    const { email, password } = this.state;
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  let navigate = useNavigate();
+  function handleClick() {
     const correctLogin = login(email, password);
+    if ((correctLogin.status = 200)) {
+      navigate("/twofa");
+    }
   }
-
-  state = {};
-  render() {
-    return (
-      <Box sx={{ width: "100%" }}>
-        <Grid
-          container
-          rowSpacing={5}
-          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-          alignItems="center"
-          justifyContent="center"
-          direction="column"
-          style={{ minHeight: "100vh" }}
-        >
-          <Grid item>
-            <h2>AnyDoc</h2>
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              required
-              id="outlined-basic"
-              label="Email"
-              variant="outlined"
-              value={this.state.email}
-              onChange={(event) => this.setState({ email: event.target.value })}
-            />
-          </Grid>
-
-          <Grid item xs={12}>
-            <TextField
-              required
-              id="outlined-basic"
-              label="Password"
-              variant="outlined"
-              onChange={(event) =>
-                this.setState({ password: event.target.value })
-              }
-            />
-          </Grid>
-
-          <Grid item xs={12}>
-            <Button
-              variant="contained"
-              onClick={(event) => this.handleClick(event)}
-            >
-              <LoginSharpIcon sx={{ mr: 2 }}></LoginSharpIcon> Login
-            </Button>
-          </Grid>
-
-          <Grid item>
-            <Button variant="outlined">Forgot Password?</Button>
-          </Grid>
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Grid
+        container
+        rowSpacing={5}
+        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+        alignItems="center"
+        justifyContent="center"
+        direction="column"
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item>
+          <h2>Login</h2>
         </Grid>
-      </Box>
-    );
-  }
+        <Grid item xs={12}>
+          <TextField
+            required
+            color="info"
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            color="info"
+            required
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <Button variant="contained" onClick={handleClick} color="secondary">
+            <LoginSharpIcon sx={{ mr: 2 }}></LoginSharpIcon> Login
+          </Button>
+        </Grid>
+
+        <Grid item>
+          <Button variant="outlined" color="info">
+            Forgot Password?
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
 
 export default Login;
---
+++ src/twofa.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import { TextField, Grid, Box } from "@mui/material";
+import LoginSharpIcon from "@mui/icons-material/LoginSharp";
+import { verifyCodeSMS, getUserInfo } from "./api/auth";
+import { userInformation } from "./api/localServices";
+import { useNavigate } from "react-router-dom";
+
+function Twofa() {
+  let navigate = useNavigate();
+  const [code, setCode] = useState("");
+
+  async function handleClick() {
+    let correctCode = await verifyCodeSMS(code);
+    if (correctCode.status === 200) {
+      let userinfo = await getUserInfo();
+
+      if ((userinfo.status = 200)) {
+        userInformation();
+        navigate("/dashboard");
+      }
+    }
+  }
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Grid
+        container
+        rowSpacing={5}
+        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+        alignItems="center"
+        justifyContent="center"
+        direction="column"
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item>
+          <h2>Two Factor Authentication</h2>
+          <p>Please enter the six digit code sent to your phone.</p>
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            required
+            color="info"
+            id="outlined-basic"
+            label="6 Digit Code"
+            variant="outlined"
+            type="number"
+            onChange={(event) => setCode(event.target.value)}
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <Button color="secondary" variant="contained" onClick={handleClick}>
+            <LoginSharpIcon sx={{ mr: 2 }}></LoginSharpIcon> Submit
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
+
+export default Twofa;
