f87edc4e5b063cbc1ce240432787d43aa38d07f2
Luis Arroyo <Luis.Arroyo@xerox.com>
2022-06-02 13:37:43 -0600

    Created document pages

---
+++ src/completed.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+class Completed extends Component {
+  state = {};
+  render() {
+    return <h1>Completed Documents</h1>;
+  }
+}
+
+export default Completed;
---
+++ src/pending.jsx
@@ -0,0 +1,5 @@
+function Pending() {
+  return <h1>Pending</h1>;
+}
+
+export default Pending;
