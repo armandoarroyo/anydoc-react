f8a9228f970e84ac1b6ed8dc638fed894bb474e6
Luis Arroyo <Luis.Arroyo@xerox.com>
2022-06-02 13:39:26 -0600

    Created routes and router logic

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Login from "./login";
+import Twofa from "./twofa";
+import Dashboard from "./dashboard";
+import Profile from "./profile";
+import Pending from "./pending";
+import Completed from "./completed";
+import ChangePassword from "./changePassword";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="twofa" element={<Twofa />} />
+      <Route path="/" element={<App />}>
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="pending" element={<Pending />} />
+        <Route path="completed" element={<Completed />} />
+        <Route path="change_password" element={<ChangePassword />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
