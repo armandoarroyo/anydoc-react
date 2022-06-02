3c2a90968fbb0faed39292d488fb353c2f1df984
Luis Arroyo <Luis.Arroyo@xerox.com>
2022-06-02 13:36:18 -0600

    Added theme

--- src/twofa.jsx
+++ src/twofa.jsx
@@ -5,8 +5,10 @@ import LoginSharpIcon from "@mui/icons-material/LoginSharp";
 import { verifyCodeSMS, getUserInfo } from "./api/auth";
 import { userInformation } from "./api/localServices";
 import { useNavigate } from "react-router-dom";
-
+import { ThemeProvider } from "@mui/material/styles";
+import customTheme from "./customTheme";
 function Twofa() {
+  let theme = customTheme();
   let navigate = useNavigate();
   const [code, setCode] = useState("");
 
@@ -22,40 +24,42 @@ function Twofa() {
     }
   }
   return (
-    <Box sx={{ width: "100%" }}>
-      <Grid
-        container
-        rowSpacing={5}
-        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-        alignItems="center"
-        justifyContent="center"
-        direction="column"
-        style={{ minHeight: "100vh" }}
-      >
-        <Grid item>
-          <h2>Two Factor Authentication</h2>
-          <p>Please enter the six digit code sent to your phone.</p>
-        </Grid>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ width: "100%" }}>
+        <Grid
+          container
+          rowSpacing={5}
+          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          alignItems="center"
+          justifyContent="center"
+          direction="column"
+          style={{ minHeight: "100vh" }}
+        >
+          <Grid item>
+            <h2>Two Factor Authentication</h2>
+            <p>Please enter the six digit code sent to your phone.</p>
+          </Grid>
 
-        <Grid item xs={12}>
-          <TextField
-            required
-            color="info"
-            id="outlined-basic"
-            label="6 Digit Code"
-            variant="outlined"
-            type="number"
-            onChange={(event) => setCode(event.target.value)}
-          />
-        </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              color="info"
+              id="outlined-basic"
+              label="6 Digit Code"
+              variant="outlined"
+              type="number"
+              onChange={(event) => setCode(event.target.value)}
+            />
+          </Grid>
 
-        <Grid item xs={12}>
-          <Button color="secondary" variant="contained" onClick={handleClick}>
-            <LoginSharpIcon sx={{ mr: 2 }}></LoginSharpIcon> Submit
-          </Button>
+          <Grid item xs={12}>
+            <Button color="secondary" variant="contained" onClick={handleClick}>
+              <LoginSharpIcon sx={{ mr: 2 }}></LoginSharpIcon> Submit
+            </Button>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
 
