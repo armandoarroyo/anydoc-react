84a3a2bade633dbf96340f7b763633c93e932465
Luis Arroyo <Luis.Arroyo@xerox.com>
2022-06-02 13:33:14 -0600

    Added put method to update user information

--- src/api/localServices.js
+++ src/api/localServices.js
@@ -1,3 +1,7 @@
+import axios from "axios";
+const authApiUrl =
+  "https://pad2zt6ubf.execute-api.us-east-1.amazonaws.com/Prod";
+
 export async function authHeader() {
   const user = localStorage.getItem("user");
   if (user && localStorage.token) {
@@ -7,6 +11,28 @@ export async function authHeader() {
   } else {
     return {};
   }
+}
+export async function changeInformation(phoneCode, phoneNumber) {
+  const url = authApiUrl + "/api/General/ChangeInformation";
+  const body = {
+    PhoneCode: phoneCode,
+    PhoneNumber: phoneNumber,
+  };
+  const headers = {
+    Authorization: "Bearer " + localStorage.token,
+  };
+  return await axios
+    .put(url, body, { headers })
+    .then(function (response) {
+      if (response.status === 200) {
+        localStorage.setItem("countryCode", phoneCode);
+        localStorage.setItem("phone", phoneNumber);
+      }
+      return response;
+    })
+    .catch(function (error) {
+      return error;
+    });
 }
 
 export async function userInformation() {
