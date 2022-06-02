a6b013e5f3e0bb54534922a80bc48a2ad454701f
Luis Arroyo <Luis.Arroyo@xerox.com>
2022-06-02 13:40:26 -0600

    Created app page display logic

--- src/App.js
+++ src/App.js
@@ -1,46 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
 import "@fontsource/roboto-condensed";
-import Login from "./login";
-import Twofa from "./twofa";
-import Dashboard from "./dashboard";
+import { Outlet, Link } from "react-router-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./navbar";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
+import Login from "./login";
+import customTheme from "./customTheme";
 
 function App() {
-  const theme = createTheme({
-    typography: {
-      fontFamily: "Roboto Condensed",
-    },
-    palette: {
-      primary: {
-        light: "#ffffff",
-        main: "#fff",
-        dark: "#cccccc",
-        contrastText: "#000",
-      },
-      secondary: {
-        light: "#ff5d54",
-        main: "#db1e2a",
-        dark: "#a10001",
-        contrastText: "#fff",
-      },
-    },
-  });
+  let theme = customTheme();
+
   if (!localStorage.token) {
     return <Login />;
   }
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="login" element={<Login />} />
-          <Route path="twofa" element={<Twofa />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="navbar" element={<NavBar />} />
-        </Routes>
-      </BrowserRouter>
+      <NavBar />
+      <Outlet />
     </ThemeProvider>
   );
 }
